01a7e75e75d78537a379e622410c3baafe1bc15d
Azael Reyes <azael.devel@gmail.com>
2022-05-20 23:27:36 -0700

    save

--- kernel/kernel.cc
+++ kernel/kernel.cc
@@ -3,12 +3,7 @@
 
 extern "C" void kernel_entry()
 {
-  	kernel::VGA video(kernel::VGA::BLACK,kernel::VGA::BLUE);
+  	kernel::VGA video(kernel::VGA::BLACK,kernel::VGA::WHITE);
   	
-  	/*video.cell(0).letter = 'H';
-  	video.cell(1).letter = 'e';
-  	video.cell(2).letter = 'l';
-  	video.cell(3).letter = 'l';
-  	video.cell(4).letter = 'o';*/
-  	video.write("Hello world...");
+  	video.write("iniciando...");
 }
