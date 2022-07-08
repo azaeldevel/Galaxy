0d0d84e492003dfcf014370f6175075239467979
Azael Reyes <azael.devel@gmail.com>
2022-07-08 16:29:55 -0700

    save

--- arch/x86/boot.cc
+++ arch/x86/boot.cc
@@ -8,7 +8,7 @@ __asm__("jmpl $0x0000, $main\n");
 void main() 
 {
      kernel::bios::print_ln("Booting..");
-     //if(not	kernel::bios::read_disk(128/*1024k * 2 = 1M*/,0,1,2,0,0x8000)) kernel::bios::print_error("Fallo la descarga del loader...");
+     if(not	kernel::bios::read_disk(128/*1024k * 2 = 1M*/,0,1,2,0,0x8000)) kernel::bios::print_error("Fallo la descarga del loader...");
      
      //asm volatile ("jmp 0x8000;": ::);
 } 
